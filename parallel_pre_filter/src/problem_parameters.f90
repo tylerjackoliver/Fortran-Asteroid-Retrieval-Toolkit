f10561f0929458bb2bd0113160d47cf5e2874630
Jack Tyler <tyler.jackoliver@gmail.com>
2020-07-05 14:44:34 +0100

    Update problem_parameters.f90

--- parallel_pre_filter/src/problem_parameters.f90
+++ parallel_pre_filter/src/problem_parameters.f90
@@ -5,22 +5,22 @@ module problem_parameters
 
     implicit none
 
-    character(*), parameter                 :: target_dataset = '../data/2020-01-28_L2PlanarPlaneCondsSynodic.csv'      ! Input manifold conditions. num_targets x 7 (time, state - synodic)
-    character(*), parameter                 :: spk_database   = '../data/targets_to_consider'                           ! List of SPK designations to consider in the pre-filter
-    character(*), parameter                 :: week_database  = '../data/weeks_to_consider'                             ! Integer ID of weeks past 1 Jan 2025 to consider
+    character(*), parameter                 :: target_dataset = '../data/your_dataset_here.csv'                         ! Input manifold conditions. num_targets x 7 (time, state - synodic)
+    character(*), parameter                 :: spk_database   = '../data/your_targets_here'                             ! List of SPK designations to consider in the pre-filter
+    character(*), parameter                 :: week_database  = '../data/your_times_here'                               ! Integer ID of weeks past 1 Jan 2025 to consider
     character(*), parameter                 :: output_file    = '../OUTPUT.data'                                        ! Output file
 
     integer, parameter                      :: output_unit = 97                                                         ! Unit number for storing the final list of candidates
 
     character(*), parameter                 :: planetary_ephemeris      = '../data/de414.bsp'
     character(*), parameter                 :: leapseconds_file         = '../data/naif0008.tls'
-    character(*), parameter                 :: candidate_ephem_prefix   = '../data/'                                    ! Prefix for directory path to candidate ephemerides
+    character(*), parameter                 :: candidate_ephem_prefix   = '../../ephemeris_files/'                      ! Prefix for directory path to candidate ephemerides
 
-    real(kind=dp), pointer                  :: manifold_data_elements(:,:)                                              ! num_targets x 6
+    double precision, pointer               :: manifold_data_elements(:,:)                                              ! num_targets x 6
 
-    real(kind=dp)                           :: manifold_data_state(6)                                                   ! [a, e, i, O, o, M, t] of the target states
-    real(kind=dp)                           :: asteroid_elements(8)                                                     ! Orbital elements of the asteroid
-    real(kind=dp)                           :: asteroid_state(6)                                                        ! State of the asteroid
+    double precision                        :: manifold_data_state(6)                                                   ! [a, e, i, O, o, M, t] of the target states
+    double precision                        :: asteroid_elements(8)                                                     ! Orbital elements of the asteroid
+    double precision                        :: asteroid_state(6)                                                        ! State of the asteroid
 
     integer, allocatable                    :: dataset(:)                                                               ! Holds the weeks to compute
     integer, allocatable                    :: asteroid_database(:)                                                     ! Holds the candidates to compute
