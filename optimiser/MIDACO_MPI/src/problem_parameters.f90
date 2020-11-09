5542625de425fd48c13328b8662646d1807b4b26
Jack Tyler <tyler.jackoliver@gmail.com>
2020-11-09 20:03:20 +0000

    Update problem_parameters.f90

--- optimiser/MIDACO_MPI/src/problem_parameters.f90
+++ optimiser/MIDACO_MPI/src/problem_parameters.f90
@@ -25,17 +25,17 @@ module problem_parameters
     !
     character(7)                    :: targ_can                                                             ! Needs to change to fit the target *currently* being studied
     character(*),  parameter        :: datafile = &
-        '../data/planeConds.csv'
+        '../path/to/states/at/the/pi/8/section/'
 
     character(*),  parameter        :: original_orbit_data = &
-        '../data/planeConds copy.csv'
+        '../path/to/original/orbit/conditions/'
 
-    character(*),  parameter        :: target_file = '../data/targets_to_consider'
+    character(*),  parameter        :: target_file = '../data/spk_ids_to_optimise'
 
     character(*),  parameter        :: ephemeris_prefix=&
-        '../../../ephemeris_files/ephemeris_files/'
+        '../path/to/ephemeris/files/'
 
-    double precision, parameter     :: minimum_transfer_time = 1.d0* 86400                                  ! Seconds
+    double precision, parameter     :: minimum_transfer_time = 0.d0* 86400                                  ! Seconds
     double precision, parameter     :: maximum_transfer_time = 1500.d0 * 86400                              ! Seconds
     integer,       allocatable      :: targ_can_array(:)                                                    ! Placeholder for now
     integer,       parameter        :: t_end_disc = 1000                                                    ! Number of backwards-time integrations
@@ -43,7 +43,7 @@ module problem_parameters
     integer,       parameter        :: max_time = 10 * 60 ! 5 hours
     ! //////////////////////////////
 
-    real*8, POINTER                 :: dataset(:,:,:) => NULL()                                           ! t_end x n_mnfd x J x 6
+    real*8, POINTER                 :: dataset(:,:,:) => NULL()                                             ! t_end x n_mnfd x J x 6
     real*8, POINTER                 :: perturbed_conds_dataset(:,:,:) => NULL()                             ! n_mnfd x J x 6
 
     double precision                :: transfer_time
