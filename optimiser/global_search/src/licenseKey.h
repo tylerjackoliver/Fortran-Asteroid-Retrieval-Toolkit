ed5182e6cd2e9ed40ce2d35a399adc0f16d9d769
Jack Tyler <tyler.jackoliver@gmail.com>
2021-03-11 09:10:28 +0000

    Update licenseKey.h

--- optimiser/global_search/src/licenseKey.h
+++ optimiser/global_search/src/licenseKey.h
@@ -1,3 +1,3 @@
 // Contains the license key for the MIDACO version being used.
 
-char licenseKeyJack[] = "Jack_Tyler_(Uni_Southampton_UK)_______[ACADEMIC-SINGLE-USER]";
+char licenseKeyJack[] = "*****";
